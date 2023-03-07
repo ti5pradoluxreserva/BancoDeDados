5fd398c73d28290a509eeab49912a985b5a4b97b
ti5pradoluxreserva <91035361+rogeriosiq@users.noreply.github.com>
2023-03-07 10:33:39 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,11 +1,38 @@
-const express = require('express')
-const routes = require('./routes')
+// const express = require('express')
+// const routes = require('./routes')
+
+// const app = express()
+// const port = 3000
+
+// routes(app)
+
+// app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
+
+// module.exports = app
+
+import express from 'express'
+// import routes  from'./routes'
+import axios from 'axios'
 
 const app = express()
 const port = 3000
 
-routes(app)
+const api = axios.create({
+    baseURL: 'http://192.168.117.207:5030/'
+  })
+
+const setup = await api.get('/setup')
+  .then(response => {
+    return response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+// routes(app)
+
+console.log(setup)
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
-module.exports = app
+// module.exports = app
